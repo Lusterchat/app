8c76284495395ebeba7d4d2c402ce092693d10e8
relaytalk <lusterchat@gmail.com>
2026-01-18 14:56:05 +0530

    Create shayari-data.js

---
+++ offline/section1/shayari-data.js
@@ -0,0 +1,53 @@
+// Add your 10 shayaris here in this format:
+const shayaris = [
+    {
+        id: 1,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 2,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 3,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 4,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 5,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 6,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 7,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 8,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 9,
+        text: "",
+        theme: ""
+    },
+    {
+        id: 10,
+        text: "",
+        theme: ""
+    }
+];
