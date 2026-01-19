acc5054cae99981c58f69f88233122baa9afa2af
relaytalk <lusterchat@gmail.com>
2026-01-19 06:26:55 +0530

    😭 shayari-data.js

--- offline/section1/shayari-data.js
+++ offline/section1/shayari-data.js
@@ -2,52 +2,191 @@
 const shayaris = [
     {
         id: 1,
-        text: "",
+        text: `Suna hai log use aankh bhar ke dekhte hain
+So uske sheher mein kuch din theher ke dekhte hain
+
+Suna hai rabt hai usko kharab-haalon se
+So apne aap ko barbaad kar ke dekhte hain
+
+Suna hai dard ki gahak hai chashm-e-naaz uski
+So hum bhi uski gali se guzar ke dekhte hain
+
+Suna hai usko bhi hai sher-o-shayari se shaghaf
+So hum bhi mo’jize apne hunar ke dekhte hain
+
+Suna hai bole to baaton se phool jhadte hain
+Ye baat hai to chalo baat kar ke dekhte hain
+
+Suna hai raat use chaand takta rehta hai
+Sitaare baam-e-falak se utar ke dekhte hain
+
+Suna hai din ko use titliyan sataati hain
+Suna hai raat ko jugnu theher ke dekhte hain
+
+Suna hai hashr hain uski ghazal-si aankhein
+Suna hai usko hiran dasht bhar ke dekhte hain
+
+Suna hai raat se badh kar hain kakelein uski
+Suna hai shaam ko saaye guzar ke dekhte hain
+
+Suna hai uski siyah-chashmgi qayamat hai
+So usko surma-farosh aah bhar ke dekhte hain
+
+Suna hai uske labon se gulab jalte hain
+So hum bahaar pe ilzaam dhar ke dekhte hain
+
+Suna hai aaina timsal hai jabeen uski
+Jo saada dil hain use ban-sanwar ke dekhte hain
+
+Suna hai jab se hamail hain uski gardan mein
+Mizaaj aur hi laal-o-gohar ke dekhte hain
+
+Suna hai chashm-e-tasavvur se dasht-e-imkaan mein
+Palang zaawiya uski kamar ke dekhte hain
+
+Suna hai uske badan ki taraash aisi hai
+Ke phool apni qabaen katar ke dekhte hain
+
+Wo sarw-qad hai magar be-gul-e-murad nahin
+Ke us shajar pe shagufe samar ke dekhte hain
+
+Bas ik nigaah se lutta hai qaafila dil ka
+So rah-rawan-e-tamanna bhi dar ke dekhte hain
+
+Suna hai uske shabistaan se muttasil hai bahisht
+Makeen udhar ke bhi jalve idhar ke dekhte hain
+
+Ruke to gardishen uska tawaaf karti hain
+Chale to usko zamane theher ke dekhte hain
+
+Kise naseeb ke be-pairahan use dekhe
+Kabhi kabhi dar-o-deewaar ghar ke dekhte hain
+
+Kahaniyan hi sahi sab mubaalaghe hi sahi
+Agar wo khwab hai taabeer kar ke dekhte hain
+
+Ab uske sheher mein thehrein ki kooch kar jaayein
+‘Faraaz’ aao sitaare safar ke dekhte hain`,
         theme: ""
     },
     {
         id: 2,
-        text: "",
+        text: `Is se pehle ke be-wafa ho jaayein
+Kyun na ai dost hum juda ho jaayein
+
+Tu bhi heere se ban gaya patthar
+Hum bhi kal jaane kya se kya ho jaayein
+
+Tu ki yakta tha be-shumaar hua
+Hum bhi tootey to ja-ba-ja ho jaayein
+
+Hum bhi majbooriyon ka uzr karein
+Phir kahin aur mubtala ho jaayein
+
+Hum agar manzilen na ban paaye
+Manzilon tak ka raasta ho jaayein
+
+Der se soch mein hain parvaane
+Raakh ho jaayein ya hawa ho jaayein
+
+Ishq bhi khel hai naseebon ka
+Khaak ho jaayein keemiya ho jaayein
+
+Ab ke gar tu mile to hum tujh se
+Aise lipaten teri qaba ho jaayein
+
+Bandagi hum ne chhod di hai 'Faraaz'
+Kya karein log jab khuda ho jaayein`,
         theme: ""
     },
     {
         id: 3,
-        text: "",
+        text: `Karoon na yaad magar kis tarah bhulaaoon use
+Ghazal bahana karoon aur gungunaoon use
+
+Wo khaar-khaar hai shaakh-e-gulaab ki maanind
+Main zakhm-zakhm hoon phir bhi gale lagaoon use
+
+Ye log tazkire karte hain apne logon ke
+Main kaise baat karoon ab kahaan se laaoon use
+
+Magar wo zood-faramosh zood-ranj bhi hai
+Ki ruth jaaye agar yaad kuch dilaoon use
+
+Wahi jo daulat-e-dil hai wahi jo raahat-e-jaan
+Tumhaari baat pe ai naaseho gawaoon use
+
+Jo hum-safar sar-e-manzil bichhad raha hai 'Faraaz'
+Ajab nahin hai agar yaad bhi na aaoon use`,
         theme: ""
     },
     {
         id: 4,
-        text: "",
+        text: `adab ne dil ke taqāze uṭhāe haiñ kyā kyā
+havas ne shauq ke pahlū dabāe haiñ kyā kyā
+
+na jaane sahv-e-qalam hai ki shāhkār-e-qalam
+balā-e-husn ne fitne uṭhāe haiñ kyā kyā
+
+nigāh ḍāl dī jis par vo ho gayā andhā
+nazar ne raṅg-e-tasarruf dikhāe haiñ kyā kyā
+
+isī fareb ne mārā ki kal hai kitnī dūr
+is aaj kal meñ abas din gaṅvāe haiñ kyā kyā
+
+pahāṛ kāṭne vāle zamīñ se hār ga.e
+isī zamīn meñ dariyā samāe haiñ kyā kyā
+
+guzar ke aap se ham aap tak pahuñch to ga.e
+magar ḳhabar bhī hai kuchh pher khāe haiñ kyā kyā
+
+buland ho to khule tujh pe zor pastī kā
+baṛe-baṛoñ ke qadam ḍagmaḡāe haiñ kyā kyā
+
+ḳhushī meñ apne qadam chūm lūñ to zebā hai
+vo laġhzishoñ pe mirī muskurāe haiñ kyā kyā
+
+ḳhudā hī jāne 'yagānā' maiñ kaun hūñ kyā hūñ
+ḳhud apnī zāt pe shak dil meñ āe haiñ kyā kyā`,
         theme: ""
     },
     {
         id: 5,
-        text: "",
+        text: `ujāle apnī yādoñ ke hamāre saath rahne do
+na jaane kis galī meñ zindagī kī shaam ho jaa.e
+`,
         theme: ""
     },
     {
         id: 6,
-        text: "",
+        text: `ranjish hī sahī dil hī dukhāne ke liye aa
+aa phir se mujhe chhoḌ ke jaane ke liye aa
+`,
         theme: ""
     },
     {
         id: 7,
-        text: "",
+        text: `achchhā ḳhāsā baiThe baiThe gum ho jaatā huuñ
+ab maiñ aksar maiñ nahīñ rahtā tum ho jaatā huuñ
+`,
         theme: ""
     },
     {
         id: 8,
-        text: "",
+        text: `Kuch log yunhi shehar mein humse bhi khafa rehte hain,
+Hum jinhe dekh kar jeete hain, wohi humse juda rehte hain`,
         theme: ""
     },
     {
         id: 9,
-        text: "",
+        text: `Dil na-ummeed to nahi, nakaam hi to hai,
+Lambi hai gham ki shaam, magar shaam hi to hai`,
         theme: ""
     },
     {
         id: 10,
-        text: "",
+        text: `Sitaaron se aage jahaan aur bhi hain,
+abhi ishq ke imtihaan aur bhi hain.`,
         theme: ""
     }
 ];
